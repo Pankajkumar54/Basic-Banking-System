35cfcd284b5a69449e369065fc7f805b06d83432
Pankajkumar <74944657+Pankajkumar54@users.noreply.github.com>
2022-03-18 01:14:43 +0530

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -1,5 +1,4 @@
-<!-- navbar --> 
-      <nav class="navbar navbar-expand-md navbar-light bg-light">
+<nav class="navbar navbar-expand-md navbar-light bg-light">
       <a class="navbar-brand" href="index.php" style="color :#292929;"> <b> BANKING SYSTEM</b></a>
       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
         <span class="navbar-toggler-icon"></span>
